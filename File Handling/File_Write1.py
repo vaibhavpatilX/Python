8ba512ebb85071850e957dd58718737b45d15272
vaibhavpatilX <119148794+vaibhavpatilX@users.noreply.github.com>
2024-06-23 01:44:08 +0530

    Add files via upload

---
+++ File Handling/File_Write1.py
@@ -0,0 +1,24 @@
+import os
+
+def main():
+    print("Enter the name of file that you want to open for writting purpose : ")
+    File_name = input()
+
+    if os.path.exists(File_name):
+        fobj = open(File_name,"w")
+        if fobj:
+            print("File successfully opened in the writing mode..!")
+
+            print("Enter the data that you want to write in the file")           
+            Data = input()
+
+            fobj.write(Data)
+            fobj.close()
+        else:
+            print("Unable to open file..!")
+    else:
+        print("There is no such file..!")
+    
+
+if __name__ == "__main__":
+    main()
